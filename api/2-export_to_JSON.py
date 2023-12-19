04378105abd8fa499cb6f5baac89aef98e027e5b
JeremMac <macal.jerem@gmail.com>
2023-12-19 14:30:04 +0100

    Added 2-export_to_JSON.py file.

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Gather data from an API for a given employee
+ID and display TODO list progress. Export data to JSON."""
+import json
+import requests
+import sys
+
+
+def to_do(employee_ID):
+    """
+    Retrieve employee information and TODO
+    list progress based on the employee ID.
+
+    Args:
+        employee_ID (int): The ID of the employee.
+
+    Returns:
+        None
+
+    Prints:
+        Displays the employee's TODO list progress.
+    """
+    url = 'https://jsonplaceholder.typicode.com'
+    employee_url = f"{url}/users/{employee_ID}"
+    todos_url = f"{url}/todos?userId={employee_ID}"
+
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+
+    if employee_response.status_code == 200:
+        employee_name = employee_data.get('username')
+
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    if todos_response.status_code == 200:
+        total_tasks = len(todos_data)
+        completed_tasks = 0
+    for task in todos_data:
+        completed_tasks += task['completed']
+
+    # create a dictionnary to put our data in
+    employee_tasks = []
+    for task in todos_data:
+        employee_tasks.append({
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": employee_name
+        })
+
+    # appending datas into the json file 
+    json_path = f"{employee_ID}.json"
+    with open(json_path, 'w') as json_file:
+        json.dump({str(employee_ID): employee_tasks}, json_file)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    to_do(employee_id)
